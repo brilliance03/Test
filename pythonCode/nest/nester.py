637ab4c707f9d26afade0e3a74ed3f53a0a4c7e0
Zheng Li <Brilliance.Lee@gmail.com>
2014-08-13 20:02:09 +0800

    improve the function print_lol()

--- pythonCode/nest/nester.py
+++ pythonCode/nest/nester.py
@@ -1,14 +1,20 @@
 '''this is the "nester.py" module and it provides one function
     called print_lol() which prints lists that may or may not
     include nested lists.'''
-def print_lol(the_list):
+
+'''edition 2 add another param called level'''
+
+def print_lol(the_list, indentation=False, level=0):
     '''this function takes one positional argument called "the_list", which
         is amy Python list (of - possibly - nested lists). Each data item in
         the provided list is (recursively) printed to the screen on it's own
         line'''
     for each_item in the_list:
         if isinstance(each_item,list):
-            print_lol(each_item)
+            print_lol(each_item,indentation,level+1)
         else:
+            if indentation:
+                for num in range(level):
+                    print("\t", end="")
             print(each_item)
             
