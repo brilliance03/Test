275bfe11830acc2c0b7c68757a5c2fcffc0f9ab5
Zheng Li <Brilliance.Lee@gmail.com>
2014-08-14 17:43:27 +0800

    learn chapter4 & 5

--- pythonCode/chapter3/sketch.py
+++ pythonCode/chapter3/sketch.py
@@ -1,16 +1,60 @@
-data = open("sketch.txt")
+import nester
+import pickle
 
-separator = ":"
+try:
+    data = open("sketch.txt")
 
-data.seek(0)
+    separator = ":"
 
-for each_line in data:
-    if each_line.find(separator) >= 0:
-        (role, line_spoken) = each_line.split(separator,1)
-        print(role, end="")
-        print(" said", end="")
-        print(line_spoken, end="")
-    else:
-        continue
+    data.seek(0)
 
-data.close()
+    man = []
+    other = []
+
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(separator,1)
+            line_spoken = line_spoken.strip()
+            if role == 'Man':
+                man.append(line_spoken)
+            elif role == 'Other Man':
+                other.append(line_spoken)
+            '''print(role, end="")
+            print(" said", end="")
+            print(line_spoken, end="")'''
+        except ValueError:
+            pass
+
+    data.close()
+
+except IOError:
+    print("There has no such file.")
+
+
+print(man)
+print(other)
+## code in normal
+##try:
+##    man_dataOut = open("man_data.txt","w")
+##    other_dataOut = open("other_data.txt","w")
+##
+##    print(man,file=man_dataOut)
+##    print(other,file=other_dataOut)
+##
+##except IOError:
+##    print("Output ERROR")
+##finally:
+##    man_dataOut.close()
+##    other_dataOut.close()
+
+'''using <with> which can help you close file automatically'''
+try:
+    with open('man_data.txt','wb') as man_file, open('other_data.txt','wb') as other_file:
+        #nester.print_lol(man,output=man_file)
+        #nester.print_lol(other,output=other_file)
+        pickle.dump(man,man_file)
+        pickle.dump(other,other_file)
+except IOError as err:
+    print('file error: ' + str(err))
+except pickle.PickleError as perr:
+    print('Pickling error: ' + str(perr))
--- pythonCode/nest/nester.py
+++ pythonCode/nest/nester.py
@@ -3,18 +3,21 @@
     include nested lists.'''
 
 '''edition 2 add another param called level'''
+'''edition 3 add another param called indentation'''
+'''edition 4 add another param called ouput'''
+import sys
 
-def print_lol(the_list, indentation=False, level=0):
+def print_lol(the_list, indentation=False, level=0, output=sys.stdout):
     '''this function takes one positional argument called "the_list", which
         is amy Python list (of - possibly - nested lists). Each data item in
         the provided list is (recursively) printed to the screen on it's own
         line'''
     for each_item in the_list:
         if isinstance(each_item,list):
-            print_lol(each_item,indentation,level+1)
+            print_lol(each_item,indentation,level+1,output)
         else:
             if indentation:
                 for num in range(level):
-                    print("\t", end="")
-            print(each_item)
+                    print("\t", end="",file=output)
+            print(each_item,file=output)
             
