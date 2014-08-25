ec325d91c66407b84ff65f0a4debfc629f39b6c3
Zheng Li <Brilliance.Lee@gmail.com>
2014-08-25 18:01:46 +0800

    learning create a class by myself

--- pythonCode/chapter5/coach.py
+++ pythonCode/chapter5/coach.py
@@ -1,3 +1,20 @@
+class Athlete:
+    def __init__(self, a_name, a_dob = None, a_times=[]):
+        self.name = a_name
+        self.dob = a_dob
+        self.times = a_times
+
+    def top3(self):
+        return(sorted(set(sanitize(t) for t in self.times))[0:3])
+
+    def add_time(self,time):
+        self.times.append(time)
+
+    def add_times(self,times):
+        self.times.extend(times)
+
+    
+
 def sanitize(time_string):
     if '-' in time_string:
         splitter = '-'
@@ -17,11 +34,15 @@ def get_coach_Data(filename):
             data = f.readline()
         #return(data.strip().split(','))
             dataList = data.strip().split(',')
+            '''
             dataDict = {'name':dataList.pop(0),
                         'dob':dataList.pop(0),
                         'times':dataList,
                         'best3':sorted(set(sanitize(t) for t in dataList))[0:3]}
-            return(dataDict)
+            return(dataDict)'''
+            return(Athlete(dataList.pop(0),
+                           dataList.pop(0),
+                           dataList))
     except IOError as ioerr:
         print('File error: ' + str(ioerr))
         return(None)
@@ -41,5 +62,8 @@ def format_datalist(datalist):
 sarah = get_coach_Data('sarah2.txt')
 #(sarah_name,sarah_dob) = sarah.pop(0),sarah.pop(0)
 ##sarahDict = {'name':sarah.pop(0),'dob':sarah.pop(0),'times':sarah}
-print(sarah['name'] + "'s fastest time are: " + str(sarah['best3']))
-
+##print(sarah['name'] + "'s fastest time are: " + str(sarah['best3']))
+print(sarah.name + "'s fastest time are: " + str(sarah.top3()))
+sarah.add_times(['1.11','3:12','4-15'])
+sarah.add_time('1.00')
+print(sarah.name + "'s fastest time are: " + str(sarah.top3()))
